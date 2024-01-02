1a5ebcea6f9337607c108cdee256b61fd0828ab5
Fervicmar <fervicmardlagman@gmail.com>
2024-01-02 18:15:20 +0800

    Update day-13.js

---
+++ frontend-lessons/javascript-tasks/day-13/day-13.js
@@ -0,0 +1,47 @@
+// JavaScript Fundamentals - Day 13: Assignment
+
+// Task 1: Variable Mastery
+
+let currentLesson ='JavaScript Fundamentals';
+console.log(currentLesson);
+
+// Task 2: Data Type Exploration
+
+let favoriteNumber = 29;
+let favoriteWord = 'hello';
+let isJavaScriptFun = true;
+let nothing = null;
+let undefineVar;
+
+// Task 3: Logical Operators in Action
+
+let isMorning = true;
+let isRaining = false;
+
+// AND operator
+
+if (isMorning && isRaining) {
+     console.log("It is morning and raining");
+}
+else {
+    console.log("It's not morning and raining");
+}
+
+// OR operator
+
+if (isMorning || isRaining) {
+    console.log("It's morning or raining");
+}
+else {
+    console.log("It's not morning or raining");
+}
+
+// NOT operator
+
+console.log(!isMorning);
+
+
+
+
+
+
